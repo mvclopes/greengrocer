37106df2ed44fd20c87042dbf054c3c5d60f7b4b
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-13 20:55:40 -0300

    Feature: complemented product details screen's layout

--- lib/src/features/product_detail/product_detail_screen.dart
+++ lib/src/features/product_detail/product_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/color_palette.dart';
 import 'package:greengrocer/src/domain/model/product.dart';
+import 'package:greengrocer/src/utils/utils.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   final Product product;
@@ -10,24 +12,114 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Image.asset(product.imageUrl),
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(45),
+          Column(
+            children: [
+              Expanded(
+                child: Hero(
+                  tag: product.imageUrl,
+                  child: Image.asset(
+                    product.imageUrl,
+                  ),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade600,
-                    offset: const Offset(0, 2) // to simulate an elevation
-                  )
-                ]
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(32),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.shade600,
+                            offset:
+                                const Offset(0, 2) // to simulate an elevation
+                            )
+                      ]),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // Name - Product quantity
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.name,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
+                          ),
+                          Container(
+                            width: 70,
+                            height: 30,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+
+                      // Price
+                      Text(
+                        Utils.convertToCurrency(amount: product.price),
+                        style: TextStyle(
+                            color: ColorPalette.swatchColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+
+                      // Description
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: SingleChildScrollView(
+                            physics: const BouncingScrollPhysics(),
+                            child: Text(
+                              product.description,
+                              style: const TextStyle(height: 1.5),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // Button
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16))),
+                          label: const Text(
+                            "Adicionar ao carrinho",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          icon: const Icon(
+                            Icons.add_shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 8,
+            left: 8,
+            child: SafeArea(
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                ),
               ),
             ),
           ),
