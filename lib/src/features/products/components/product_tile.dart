5729bf9454a657aaa95ed6c8f18ecfd9aa169061
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-13 11:00:07 -0300

    Feature: finished product tile layout

--- lib/src/features/products/components/product_tile.dart
+++ lib/src/features/products/components/product_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/color_palette.dart';
 import 'package:greengrocer/src/domain/model/product.dart';
+import 'package:greengrocer/src/utils/utils.dart';
 
 class ProductTile extends StatelessWidget {
   final Product product;
@@ -9,42 +10,71 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Image(image: )
-            Expanded(child: Image.asset(product.imageUrl)),
-            Text(
-              product.name,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                // Image(image: )
+                Expanded(child: Image.asset(product.imageUrl)),
                 Text(
-                  product.price.toString(),
-                  style: TextStyle(
-                      color: ColorPalette.swatchColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
+                  product.name,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                  " /${product.unit.name}",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: 12,
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      Utils.convertToCurrency(amount: product.price),
+                      style: TextStyle(
+                          color: ColorPalette.swatchColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      " /${product.unit.name}",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: ColorPalette.swatchColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
