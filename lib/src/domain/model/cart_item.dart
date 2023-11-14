cedfd736aad0abded63feae15715ac482636ea34
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-14 12:20:34 -0300

    Feature: created model to cart item

---
+++ lib/src/domain/model/cart_item.dart
@@ -0,0 +1,11 @@
+import 'package:greengrocer/src/domain/model/product.dart';
+
+class CartItem {
+  Product product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    required this.quantity,
+  });
+}
