aea9e96c0173e7658f233be0bfd2b4205d22326d
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-14 17:35:11 -0300

    Feature: created profile forms component

---
+++ lib/src/features/profile/components/profile_forms.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/features/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/utils/common_input_masks.dart';
+
+class ProfileForms extends StatelessWidget {
+  const ProfileForms({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Email
+        const CustomTextField(icon: Icons.email, labelText: "Email"),
+
+        // Name
+        const CustomTextField(icon: Icons.person, labelText: "Nome"),
+
+        // Phone number
+        CustomTextField(
+          icon: Icons.phone,
+          labelText: "Celular",
+          inputFormatters: [CommonInputMasks.phoneFormatter],
+          keyboardType: TextInputType.number,
+        ),
+
+        // Name
+        CustomTextField(
+          icon: Icons.file_copy,
+          labelText: "CPF",
+          isObscuredField: true,
+          inputFormatters: [CommonInputMasks.cpfFormatter],
+          keyboardType: TextInputType.number,
+        ),
+      ],
+    );
+  }
+}
