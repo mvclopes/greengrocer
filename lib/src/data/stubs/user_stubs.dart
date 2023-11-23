314a9831b5fee3d47885810d8fb141accf6b2981
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-11-22 22:56:52 -0300

    Feature: created user model and its stub

---
+++ lib/src/data/stubs/user_stubs.dart
@@ -0,0 +1,9 @@
+import 'package:greengrocer/src/domain/model/user.dart';
+
+User userStub = User(
+  name: "Fulano",
+  email: "fulano@email.com",
+  phoneNumber: "11 99999-9999",
+  cpf: "123.456.789-01",
+  password: "password"
+);
---
+++ lib/src/domain/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String name;
+  String email;
+  String phoneNumber;
+  String cpf;
+  String password;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.cpf,
+    required this.password
+  });
+}
